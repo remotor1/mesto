21904bc8d39aa7b104a3793d1d0352a8310bb64f
Igor Savin <remotor-work@yandex.ru>
2021-01-30 23:33:40 -0500

    mesto1

---
+++ scripts.js
@@ -0,0 +1,48 @@
+let editButton = document.querySelector('.profile__edit-button');
+let closeButton = document.querySelector('.popup__close-button')
+let popup = document.querySelector('.popup');
+let profileTitle = document.querySelector('.profile__title');
+let profileSubtitle = document.querySelector('.profile__subtitle');
+let formElement = document.querySelector('.popup__container');
+let nameInput = formElement.querySelector('.popup__name');
+let jobInput = formElement.querySelector('.popup__info');
+
+
+// Закрытие открытие попап
+
+editButton.addEventListener('click', function () {
+  popup.classList.add('popup_opened');
+  nameInput.value = profileTitle.textContent;
+  jobInput.value = profileSubtitle.textContent;
+}); 
+
+closeButton.addEventListener('click', function() {
+  popup.classList.remove('popup_opened');
+});
+
+function formSubmitHandler(evt) {
+  profileTitle.textContent = nameInput.value;
+  profileSubtitle.textContent = jobInput.value;
+  popup.classList.remove('popup_opened');
+  event.preventDefault();
+}
+
+formElement.addEventListener('submit', formSubmitHandler);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
